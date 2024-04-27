560684a082b22c8c168b3d463b9cd0d3766f4420
Ahsan Raza <70075564+Ahsan668@users.noreply.github.com>
2024-04-28 02:26:10 +0500

    Update info_handler.dart

--- lib/infoHandler/info_handler.dart
+++ lib/infoHandler/info_handler.dart
@@ -9,27 +9,32 @@ class AppInfo extends ChangeNotifier{
   int countTotalTrips = 0;
   List<String> historyTripsKeyList = [];
   List<TripHistoryModel> allTripsHistoryInformationList = [];
-
+  
+  //update pickup location 
   void updatePickUpLocationAddress(Directions userPickUoAddress){
     userPickUpLocation = userPickUoAddress;
     notifyListeners();
   }
-
+  
+  //update dropoff location
   void updateDropOffLocationAddress(Directions dropOffAddress){
     userDropOffLocation = dropOffAddress;
     notifyListeners();
   }
 
+  //update all trip counter
   updateOverallTripCounter(int overAllTripCounter){
     countTotalTrips = overAllTripCounter;
     notifyListeners();
   }
 
+  //update all trip keys
   updateOverAllTripKeys(List<String> tripsKeysList){
     historyTripsKeyList = tripsKeysList;
     notifyListeners();
   }
-
+  
+  //update all trip history info
   updateOverAllTripHistoryInformation(TripHistoryModel eachTripHistory){
     allTripsHistoryInformationList.add(eachTripHistory);
     notifyListeners();
