3d897225ee25226f44083e07cc3c6aa2cb3777d6
Ahsan Raza <70075564+Ahsan668@users.noreply.github.com>
2024-04-28 02:14:02 +0500

    Update and rename grofire_assistant.dart to geofire_assistant.dart

--- lib/Assistants/geofire_assistant.dart
+++ lib/Assistants/geofire_assistant.dart
@@ -3,12 +3,14 @@ import '../models/active_nearby_available_drivers.dart';
 class GeoFireAssistant {
   static List<ActiveNearByAvailableDrivers> activeNearByAvailableDriversList = [];
 
+    //remove offline driver from online list
   static void deleteOfflineDriverFromList(String driverId){
     int indexNumber = activeNearByAvailableDriversList.indexWhere((element) => element.driverId == driverId);
 
     activeNearByAvailableDriversList.removeAt(indexNumber);
   }
-
+  
+  //update nearby active drivers
   static void updateActiveNearByAvailableDriverLocation(ActiveNearByAvailableDrivers driverWhoMove) {
     int indexNumber = activeNearByAvailableDriversList.indexWhere((element) => element.driverId == driverWhoMove.driverId);
 
